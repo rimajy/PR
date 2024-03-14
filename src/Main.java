5215b0856b29b21254c9c3b77b38812faf68e18f
rimajy <157909303+rimajy@users.noreply.github.com>
2024-03-14 13:50:17 +0200

    main

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Зчитуємо рядок зі стандартного вводу
+        String input = scanner.nextLine();
+
+        // Розділяємо рядок на числа за пробілами
+        String[] numbersArray = input.split("\\s+");
+        List<Integer> numbers = new ArrayList<>();
+
+        // Конвертуємо рядок чисел в список цілих чисел
+        for (String numStr : numbersArray) {
+            int number = Integer.parseInt(numStr);
+            numbers.add(number);
+        }
+
+
+    }
+}
