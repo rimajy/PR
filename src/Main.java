ca2f5aecb0a1bc8614cef40c6cca2c79e17ef994
rimajy <157909303+rimajy@users.noreply.github.com>
2024-03-14 14:01:20 +0200

    decimalToBinary and sort

--- src/Main.java
+++ src/Main.java
@@ -11,10 +11,49 @@ public class Main {
         String[] numbersArray = input.split("\\s+");
         List<Integer> numbers = new ArrayList<>();
 
-        // Конвертуємо рядок чисел в список цілих 
+        // Конвертуємо рядок чисел в список цілих чисел
         for (String numStr : numbersArray) {
             int number = Integer.parseInt(numStr);
             numbers.add(number);
+        }
+        // Конвертуємо числа в бінарне представлення у доповнювальному коді
+        List<String> binaryStrings = new ArrayList<>();
+        for (int num : numbers) {
+            binaryStrings.add(decimalToBinary(num));
+        }
+
+        // Сортуємо бінарні значення методом бульбашкового сортування
+        bubbleSort(binaryStrings);
+
+        // Виводимо відсортовані бінарні значення
+        for (String binary : binaryStrings) {
+            System.out.println(binary);
+        }
+
+    }
+    // Метод для конвертації десяткового числа в бінарне представлення у доповнювальному коді
+    public static String decimalToBinary(int n) {
+        int limit = (int) Math.pow(2, 15) - 1; // Максимальне значення для 16-бітного представлення
+        if (n > limit) {
+            n = limit;
+        } else if (n < -limit) {
+            n = -limit;
+        }
+        return String.format("%16s", Integer.toBinaryString(n & 0xFFFF)).replace(' ', '0');
+    }
+
+    // Метод для сортування списку методом бульбашкового сортування
+    public static void bubbleSort(List<String> list) {
+        int n = list.size();
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (list.get(j).compareTo(list.get(j + 1)) > 0) {
+                    // Swap
+                    String temp = list.get(j);
+                    list.set(j, list.get(j + 1));
+                    list.set(j + 1, temp);
+                }
+            }
         }
     }
 }
